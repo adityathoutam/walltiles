4fbe1b5a0977ee79b2f4983c3d4cd0c45d648075
Aditya Thoutam <adityathoutam@outlook.com>
2018-03-29 10:13:45 +0000

    No Comments

--- Assets/Scripts/WallCreator.cs
+++ Assets/Scripts/WallCreator.cs
@@ -8,65 +8,130 @@ public class WallCreator : MonoBehaviour {
 	public float RowWidth;
 	public float ColoumnWidth;
 	Vector2 scale;
-	
-	//row_element = 	-boxCollider.bounds.min.y-scale.y/2-ColoumnWidth
-	//1st row y =  	row_element-rowNumber; //0
-	//2nd row y = 	row_element-rowNumber; //1
+
+    GameObject[] TileArray;
+    GameObject[] TileArray2;
 
 
-	//1st x = boxCollider.bounds.min.x+scale.x/2;
 
-	void Start () 
+    //1st x = boxCollider.bounds.min.x+scale.x/2;
+
+    void Start () 
 	{
 		boxCollider = GetComponent<BoxCollider>();
 		scale = transform.GetChild(0).localScale;
-	}
+        TileArray = new GameObject[transform.childCount];
+        TileArray2 = new GameObject[transform.childCount];
+    }
 	
 	
 	void Update ()
 	{
-		 transform.GetChild(0).position = new Vector3(boxCollider.bounds.min.x+scale.x/2,
-													-boxCollider.bounds.min.y-scale.y/2,0);
-
-		// transform.GetChild(0).position = new Vector3(0,
-		// 											 -boxCollider.bounds.min.y-scale.y/2-1.5f-ColoumnWidth,0);
-
-			
 		
-		for(int i=0;i<transform.childCount-1;i++)
-		{
+	     transform.GetChild(0).position = new Vector3(boxCollider.bounds.min.x+scale.x/2,
+													-boxCollider.bounds.min.x-scale.y/2-ColoumnWidth,0);
 
-			transform.GetChild(i+1).position = transform.GetChild(i).position;
+        //Init(10);
+        //Init(20);
+        //Init(30);
+        //Init(40);
+        //Init(50);
+        //Init(60);
+        //Init(70);
+        //Init(80);
+        //Init(90);
+        //Init(100);
 
-			transform.GetChild(i+1).position = new Vector3 (transform.GetChild(i+1).position.x+scale.x+RowWidth,
-													transform.GetChild(i+1).position.y,
-													transform.GetChild(i+1).position.z);
-													
-			if(transform.GetChild(i+1).position.x>boxCollider.bounds.max.x
-				&& transform.GetChild(i+1).position.y>=-boxCollider.bounds.min.y-scale.y/2-ColoumnWidth)
-			{
+        for (int i = 10; i < 100; i=i+10) 
+        {
+            Init(i);
+            
+        }
 
-				transform.GetChild(i+1).position = new Vector3 (boxCollider.bounds.min.x + scale.x/2,
-																-boxCollider.bounds.min.y-scale.y/2-1f,0);
+        //for (int i = 0; i < 99; i++)
+        //        SetPosition(i);
 
-			}
-			if(transform.GetChild(i+1).position.x>boxCollider.bounds.max.x
-				&& transform.GetChild(i+1).position.y>=-boxCollider.bounds.min.y-scale.y/2-1f-ColoumnWidth)
-			{
 
-				transform.GetChild(i+1).position = new Vector3 (boxCollider.bounds.min.x + scale.x/2,
-																-boxCollider.bounds.min.y-scale.y/2-2f,0);
+        for (int i = 0; i < 9; i++)
+        {
 
-			}
-				
-		}	
 
-		
-		
+            SetPosition(i);
 
-	
-	}
+        }
+        for (int i = 10; i < 19; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 20; i < 29; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 30; i < 39; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 40; i < 49; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 50; i < 59; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 60; i < 69; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 70; i < 79; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 80; i < 89; i++)
+        {
+
+            SetPosition(i);
+        }
+        for (int i = 90; i < 99; i++)
+        {
+
+            SetPosition(i);
+        }
+
+
+
+
+
+    }
+    void SetPosition(int i)
+    {
+        transform.GetChild(i +1).position = transform.GetChild(i).position;
+
+        transform.GetChild(i +1).position = new Vector3(transform.GetChild(i + 1).position.x + scale.x + RowWidth,
+                                                transform.GetChild(i +1).position.y,
+                                                transform.GetChild(i + 1).position.z);
+
+    }
+    void Init(int i)
+    {
+        transform.GetChild(i).position = new Vector3(transform.GetChild(i - 10).position.x,
+                                                 transform.GetChild(i - 10).position.y - 1f - ColoumnWidth, 0);
+
+    }
+
+
+
+
+
 }
+
 
 		
 

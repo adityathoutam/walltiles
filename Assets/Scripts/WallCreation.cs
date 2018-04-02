68569229678f1d84ea6fed83b9bc69061da15089
Aditya Thoutam <adityathoutam@outlook.com>
2018-04-02 02:48:37 +0000

    No Comments

--- Assets/Scripts/WallCreation.cs
+++ Assets/Scripts/WallCreation.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallCreator2 : MonoBehaviour {
+public class WallCreation : MonoBehaviour {
 
 	BoxCollider boxCollider;
     Vector2 scale;

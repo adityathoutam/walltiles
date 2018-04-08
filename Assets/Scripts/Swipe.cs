75a716cc0c7a288c054304e15671f858315ab27d
Aditya Thoutam <adityathoutam@outlook.com>
2018-04-08 04:57:30 +0000

    No Comments

--- Assets/Scripts/Swipe.cs
+++ Assets/Scripts/Swipe.cs
@@ -6,14 +6,17 @@ public class Swipe : MonoBehaviour
 {
 
     Vector3 Touchposition;
-    RectTransform rect;
+    RectTransform rect,rect2;
     float swipeX = 150f;
-    public GameObject Panel;
+    public GameObject Panel,Panel2;
+    Vector2 deltaSwipe;
 
     void Start()
     {
-        Panel.SetActive(true);
+        Panel.SetActive(false);
+        Panel2.SetActive(true);
         rect = Panel.GetComponent<RectTransform>();
+        rect2 = Panel2.GetComponent<RectTransform>();
     }
 
     void SwipeManager()
@@ -23,9 +26,13 @@ public class Swipe : MonoBehaviour
             Touchposition = Input.mousePosition;
         }
 
+
+    if(Touchposition.x>Screen.width/2)
+    {
         if (Input.GetMouseButtonUp(0))
         {
-            Vector2 deltaSwipe = Touchposition - Input.mousePosition;
+             deltaSwipe = Touchposition - Input.mousePosition;
+
 
             if (Mathf.Abs(deltaSwipe.x) > swipeX)
             {
@@ -41,6 +48,26 @@ public class Swipe : MonoBehaviour
 
         }
     }
+    if(Touchposition.x<Screen.width/2)
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+             deltaSwipe = Touchposition - Input.mousePosition;
+
+            Debug.Log(deltaSwipe.x);
+              if (deltaSwipe.x < 0)
+                 {
+                    SetRect(rect2, 180, 90, 240, 60);
+                }
+                 else
+                 {
+                     SetRect(rect2, -1795, 90, 2215, 60);
+                }
+             }
+
+        }
+    }
+
     public static void SetRect(RectTransform rectTransform, float left, float top, float right, float bottom)
     {
         rectTransform.offsetMin = new Vector2(left, bottom);
@@ -49,5 +76,6 @@ public class Swipe : MonoBehaviour
     void Update()
     {
        SwipeManager();
+
     }
 }

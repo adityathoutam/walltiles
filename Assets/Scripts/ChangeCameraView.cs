2a4dee9818c5c551f30b5a9137094585ec94f517
Aditya Thoutam <adityathoutam@outlook.com>
2018-04-13 08:38:10 +0000

    No Comments

--- Assets/Scripts/ChangeCameraView.cs
+++ Assets/Scripts/ChangeCameraView.cs
@@ -10,14 +10,19 @@ public class ChangeCameraView : MonoBehaviour
     public Camera mainCamera;
     public Camera ZoomCamera;
 
-    private bool CamSwitch = false;
-
+    private bool CamSwitch;
+    
+    void Awake()
+    {
+         CamSwitch = false;
+        
+    }
 
     public void SwitchZoom()
     {
         CamSwitch = !CamSwitch;
-        mainCamera.gameObject.SetActive(CamSwitch);
-        ZoomCamera.gameObject.SetActive(!CamSwitch);
+        mainCamera.gameObject.SetActive(!CamSwitch);
+        ZoomCamera.gameObject.SetActive(CamSwitch);
       
     }
 
@@ -26,8 +31,7 @@ public class ChangeCameraView : MonoBehaviour
   
     public void OnClicked()
     {
-       if(CameraMan)
-       {
+       
 
        if (firstClick)
        {
@@ -45,22 +49,22 @@ public class ChangeCameraView : MonoBehaviour
            firstClick = true;
            secondClick = true;
        }
-       }
+       
     }
 
     public void First()
     {
-       Camera.main.fieldOfView =30;
+      ZoomCamera.fieldOfView =30;
     }
 
     public void Second()
     {
-       Camera.main.fieldOfView = 20;
+       ZoomCamera.fieldOfView = 20;
     }
 
     public void Third()
     {
-       Camera.main.fieldOfView = 10;
+       ZoomCamera.fieldOfView = 10;
     }
 
 }

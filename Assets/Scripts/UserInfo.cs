6dbba6a6ccc1f74e62a68605301b59e9c1b7add0
Aditya Thoutam <adityathoutam@outlook.com>
2018-04-30 16:32:53 +0000

    No Comments

--- Assets/Scripts/UserInfo.cs
+++ Assets/Scripts/UserInfo.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class UserInfo : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public GameObject WallHeightDisplay;
+	public Slider WallHeightSlider;
+	public GameObject WallWidthDisplay;
+	public Slider WallWidthSlider;
+
+
+	public GameObject TileHeightDisplay;
+	public Slider TileHeightSlider;
+	public GameObject TileWidthDisplay;
+	public Slider TileWidthSlider;
 	
-	// Update is called once per frame
-	void Update () {
-		
+	private void Update()
+	{
+		WallHeightDisplay.GetComponent<TMP_InputField>().text=string.Format("{0:N0}",WallHeightSlider.value);
+		WallWidthDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N0}",WallWidthSlider.value);
+
+		TileHeightDisplay.GetComponent<TMP_InputField>().text=string.Format("{0:N0}",TileHeightSlider.value);
+		TileWidthDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N0}",TileWidthSlider.value);
+	
 	}
+
 }
+

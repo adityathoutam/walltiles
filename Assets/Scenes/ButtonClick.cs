12d7dbaa7702b2c6a74e739847b439f23e5b828a
Aditya Thoutam <adityathoutam@outlook.com>
2018-04-30 07:14:38 +0000

    No Comments

---
+++ Assets/Scenes/ButtonClick.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonClick : MonoBehaviour {
+
+public GameObject Panel1;
+public GameObject Panel2;
+private void Start()
+{
+	Panel1.SetActive(true);
+		Panel2.SetActive(true);
+	
+}
+	public void View1()
+	{
+		Panel1.SetActive(true);
+		Panel2.SetActive(false);
+
+	}
+	public void View2()
+	{
+		Panel2.SetActive(true);
+		Panel1.SetActive(false);
+
+	}
+}

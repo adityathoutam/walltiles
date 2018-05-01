c3b559ae9107223363b6f0144b663147e5c03515
Aditya Thoutam <adityathoutam@outlook.com>
2018-05-01 06:20:41 +0000

    No Comments

--- Assets/Scripts/UserInfo.cs
+++ Assets/Scripts/UserInfo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 
 
@@ -18,6 +19,13 @@ public class UserInfo : MonoBehaviour {
 	public GameObject TileWidthDisplay;
 	public Slider TileWidthSlider;
 
+	public Canvas canvas;
+	 GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+	 Sprite image;
+    List<RaycastResult> results = new List<RaycastResult>();
+
 	private void Update()
 	{
 		WallHeightDisplay.GetComponent<TMP_InputField>().text=string.Format("{0:N0}",WallHeightSlider.value);
@@ -27,6 +35,34 @@ public class UserInfo : MonoBehaviour {
 		TileWidthDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N0}",TileWidthSlider.value);
 	
 	}
+	 private void Awake()
+    {
+
+        m_Raycaster = canvas.GetComponent<GraphicRaycaster>();
+        m_EventSystem = GetComponent<EventSystem>();
+    }
+	void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            m_PointerEventData = new PointerEventData(m_EventSystem);
+            m_PointerEventData.position = Input.mousePosition;
+            m_Raycaster.Raycast(m_PointerEventData, results);
+            CellSelected();
+        }
+
+    }
+    void CellSelected()
+    {
+        foreach (RaycastResult result in results)
+        {
+            if (result.gameObject.tag == "ColorTile")
+            {
+                image = result.gameObject.GetComponent<Image>().sprite;
+				
+            }
+        }
+    }
 
 }
 

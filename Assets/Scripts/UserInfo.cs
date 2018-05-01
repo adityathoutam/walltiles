2ae3f3c0de7f7f48f65bd9cc6570e270ad381047
Aditya Thoutam <adityathoutam@outlook.com>
2018-05-01 06:57:40 +0000

    No Comments

--- Assets/Scripts/UserInfo.cs
+++ Assets/Scripts/UserInfo.cs
@@ -19,20 +19,33 @@ public class UserInfo : MonoBehaviour {
 	public GameObject TileWidthDisplay;
 	public Slider TileWidthSlider;
 
+	public GameObject GroutDisplay;
+	public Slider GroutSlider;
+
 	public Canvas canvas;
 	 GraphicRaycaster m_Raycaster;
     PointerEventData m_PointerEventData;
     EventSystem m_EventSystem;
-	 Sprite image;
+	 Sprite TileImage;
+	 Material TileMaterial;
+
+	 Sprite GroutImage;
+	 Material GroutMaterial;
+	 public Material[] materialList;
     List<RaycastResult> results = new List<RaycastResult>();
 
 	private void Update()
 	{
+			TileImageSelected();
+		GroutImageSelected();
+		
 		WallHeightDisplay.GetComponent<TMP_InputField>().text=string.Format("{0:N0}",WallHeightSlider.value);
 		WallWidthDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N0}",WallWidthSlider.value);
 
 		TileHeightDisplay.GetComponent<TMP_InputField>().text=string.Format("{0:N0}",TileHeightSlider.value);
 		TileWidthDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N0}",TileWidthSlider.value);
+
+		GroutDisplay.GetComponent<TMP_InputField>().text =string.Format("{0:N2}",GroutSlider.value);
 	
 	}
 	 private void Awake()
@@ -41,6 +54,7 @@ public class UserInfo : MonoBehaviour {
         m_Raycaster = canvas.GetComponent<GraphicRaycaster>();
         m_EventSystem = GetComponent<EventSystem>();
     }
+
 	void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.Mouse0))
@@ -58,10 +72,91 @@ public class UserInfo : MonoBehaviour {
         {
             if (result.gameObject.tag == "ColorTile")
             {
-                image = result.gameObject.GetComponent<Image>().sprite;
+                TileImage = result.gameObject.GetComponent<Image>().sprite;
+				
+            }
+			if (result.gameObject.tag == "GroutColor")
+            {
+                GroutImage = result.gameObject.GetComponent<Image>().sprite;
 				
             }
         }
+    }
+	void TileImageSelected()
+    {
+        if (TileImage != null)
+        {
+            if (TileImage.name == "Tile_A")
+                TileMaterial = materialList[0];
+                
+            else if (TileImage.name == "Tile_B")
+                TileMaterial = materialList[1];
+
+            else if (TileImage.name == "Tile_C")
+                TileMaterial = materialList[2];
+
+            else if (TileImage.name == "Tile_D")
+                TileMaterial = materialList[3];
+
+            else if (TileImage.name == "Tile_E")
+                TileMaterial = materialList[4];
+
+            else if (TileImage.name == "Tile_F")
+                TileMaterial = materialList[5];
+
+            else if (TileImage.name == "Tile_G")
+                TileMaterial = materialList[6];
+
+            else if (TileImage.name == "Tile_H")
+                TileMaterial = materialList[7];
+
+            else if (TileImage.name == "Tile_I")
+                TileMaterial = materialList[8];
+
+            else if (TileImage.name == "Tile_J")
+                TileMaterial = materialList[9];
+        }
+        else
+        TileMaterial = materialList[0];
+
+    }
+	void GroutImageSelected()
+    {
+        if (GroutImage != null)
+        {
+            if (GroutImage.name == "Tile_A")
+                GroutMaterial = materialList[0];
+                
+            else if (GroutImage.name == "Tile_B")
+                GroutMaterial = materialList[1];
+
+            else if (GroutImage.name == "Tile_C")
+                GroutMaterial = materialList[2];
+
+            else if (GroutImage.name == "Tile_D")
+                GroutMaterial = materialList[3];
+
+            else if (GroutImage.name == "Tile_E")
+                GroutMaterial = materialList[4];
+
+            else if (GroutImage.name == "Tile_F")
+                GroutMaterial = materialList[5];
+
+            else if (GroutImage.name == "Tile_G")
+                GroutMaterial = materialList[6];
+
+            else if (GroutImage.name == "Tile_H")
+                GroutMaterial = materialList[7];
+
+            else if (GroutImage.name == "Tile_I")
+                GroutMaterial = materialList[8];
+
+            else if (GroutImage.name == "Tile_J")
+                GroutMaterial = materialList[9];
+        }
+        else
+        GroutMaterial = materialList[0];
+
     }
 
 }

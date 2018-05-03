d6e3aae7d8e1bb1697c45d77177d252f6b62b906
Aditya Thoutam <adityathoutam@outlook.com>
2018-05-03 07:12:09 +0000

    No Comments

---
+++ Assets/TopMaker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopMaker : MonoBehaviour {
+
+	public GameObject TopWallPrefab;
+	
+	private void Awake()
+	{
+		GameObject TopWall = Instantiate(TopWallPrefab) as GameObject;
+		TopWall.transform.parent = this.transform;
+		TopWall.transform.position = new Vector3(0,5,-3.5f);
+		TopWall.transform.Rotate(-90,45,0);
+	}
+}
